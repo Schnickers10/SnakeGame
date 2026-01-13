a26c51d991fc271ecab0fb96663f3336deb177ed
Schnickers10 <ordnersofia@gmail.com>
2026-01-13 13:07:07 +0100

    We have a window that works and has a light purple background

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include "raylib.h"
 
-class snake
-{
-    public:
-    int head;
-    int tail;
-    int body;
-
-};
-
 int main()
 {
+    // Das Window definieren
+    int screenWidth{1000};
+    int screenHeight{800};
+    InitWindow(screenWidth, screenHeight, "SnakeGamey");
+    SetTargetFPS(60);
+    //Background color define
+
+
+    while(!WindowShouldClose())
+    {
+        // Game inhalt. Also alle bedingungen
+
+        //Drawing, also auf dem Hintergeund malen
+        BeginDrawing();
+        Color BackgroundColor{254, 234, 250, 100};
+        ClearBackground(BackgroundColor);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 
 }
